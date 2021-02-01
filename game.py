b0d31c0359e8f241088e879388b57a052eb522fd
Geoffrey <geoffrey.brunet@quartz-insight.com>
2021-02-01 13:45:20 +0100

    getters & setters added

--- game.py
+++ game.py
@@ -3,27 +3,55 @@ import time
 
 class Character:
     lifePoints = 100
-    actionPoints = 10
+    def get_lifePoints(self):
+        return self.lifePoints
+    def set_lifePoints(self, lifePoints):
+        self.lifePoints = lifePoints
+    attackPoints = 10
+    def get_attackPoints(self):
+        return self.attackPoints
+    def set_attackPoints(self, attackPoints):
+        self.attackPoints = attackPoints
 
 class Warrior(Character):
     lifePoints = Character.lifePoints + 20
-    actionPoints = Character.actionPoints + 10
+    def get_lifePoints(self):
+        return self.lifePoints
+    def set_lifePoints(self, lifePoints):
+        self.lifePoints = lifePoints
+    attackPoints = Character.attackPoints + 10
+    def get_attackPoints(self):
+        return self.attackPoints
+    def set_attackPoints(self, attackPoints):
+        self.attackPoints = attackPoints
 
 class Wizard(Character):
     lifePoints = Character.lifePoints
-    actionPoints = Character.actionPoints
+    def get_lifePoints(self):
+        return self.lifePoints
+    def set_lifePoints(self, lifePoints):
+        self.lifePoints = lifePoints
+    attackPoints = Character.attackPoints
+    def get_attackPoints(self):
+        return self.attackPoints
+    def set_attackPoints(self, attackPoints):
+        self.attackPoints = attackPoints
     def drinkPotion(self):
-        self.lifePoints + 15
+        self.lifePoints += 15
 
 def warriorDamage():
     ratioDmg = random.randint(1,10)/10
-    Dmg = ratioDmg
-    return Dmg
+    Dmg = Warrior.attackPoints * ratioDmg
+    attack = Wizard.set_lifePoints - Dmg
+    print(Wizard.get_lifePoints)
+    return attack
 
 def wizardDamage():
     ratioDmg = random.randint(1,10)/10
-    Dmg = ratioDmg
-    return Dmg
+    Dmg = Wizard.attackPoints * ratioDmg
+    attack = Warrior.set_lifePoints - Dmg
+    print(Warrior.get_lifePoints)
+    return attack
 
 if __name__ == "__main__":
     if Warrior.lifePoints == 0:
