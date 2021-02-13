9dbde80625978f5871593e5efda2fa71c5f7962e
Geoffrey <geoffrey.brunet@quartz-insight.com>
2021-02-13 14:37:24 +0100

    self hit and heal added

--- game.py
+++ game.py
@@ -30,10 +30,14 @@ class Warrior(Character):
         return self.attackPoints
     def set_attackPoints(self, attackPoints):
         self.attackPoints = attackPoints
+    def selfHit(self):
+        ratioDmg = random.randint(1,10)/10
+        self.set_lifePoints(self.get_lifePoints() - self.attackPoints * ratioDmg)
 
 # Wizard class
 class Wizard(Character):
     lifePoints = Character.lifePoints
+    potion = 5
     def get_lifePoints(self):
         return self.lifePoints
     def set_lifePoints(self, lifePoints):
@@ -45,17 +49,31 @@ class Wizard(Character):
         self.attackPoints = attackPoints
     def drinkPotion(self):
         self.lifePoints += 15
+        potion -= 5
+        print("Healed ! You have ", potion, "potions.")
 
 def game():
     player1 = Warrior()
     player2 = Wizard()
-    print("player 1 have ", player1.get_lifePoints ," life points")
-    print("player 1 have ", player1.get_lifePoints ," life points")
-    while player1.lifePoints >= 0 or player2.lifePoints >= 0:
+    print("player 1 have ", player1.lifePoints ," life points")
+    print("player 1 have ", player1.lifePoints ," life points")
+    if player1.lifePoints >= 0 or player2.lifePoints >= 0:
         player1.attack(player2)
+        selfHit = random.randint(1,5)
+        getHeal = random.randint(1,2)
+        if selfHit == 1:
+            print("truc ici")
+        else:
+            print("don't self hit")
         time.sleep(1)
         player2.attack(player1)
+        if getHeal == 1:
+            player2.drinkPotion()
+        else:
+            print("don't self heal")
         time.sleep(1)
+    else:
+        print("loose")
 
 
 if __name__ == "__main__":
