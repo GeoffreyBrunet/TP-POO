9a3dcb193a8d793c33ff74059b15967082c06331
Geoffrey <geoffrey.brunet@quartz-insight.com>
2021-02-14 04:22:34 +0100

    condition for launch game or leave added

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import game
+import time
 
 game = game.launchGame()
 # start game when main.py is started
 if __name__ == "__main__":
-    game.launch()
+    print("Want play to hero smash v1 ?")
+    play = input("Y: Yes, N: No, Choose: ")
+    if play == "Y" or play == "y":
+        game.launch()
+    else:
+        print("By by, have a nice week!")
+        time.sleep(1)
+        exit()
